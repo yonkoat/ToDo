fec7a50a68ba32c93408101b722741b98ceb0df4
chelik <cheldobrek3541@gmail.com>
2025-07-12 10:47:24 +0300

    Исправил ошибку с большим количеством редиректов и убрал проверку на корень сайта

--- server/auth.go
+++ server/auth.go
@@ -6,10 +6,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "./" {
-			http.Redirect(w, r, "/index.html", http.StatusSeeOther)
-			return
-		}
+
 		http.FileServer(http.Dir("./")).ServeHTTP(w, r)
 
 	})
