eca599302c6909a2ebbe5bf71b58b30a572d5b98
chelik <cheldobrek3541@gmail.com>
2025-07-13 15:18:41 +0300

    Удалил не нужные файлы и сделал обработку форм появилась ошибка с тем что редирект на страницу регистрации не работает

--- server/auth.go
+++ server/auth.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
+
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		http.FileServer(http.Dir("./")).ServeHTTP(w, r)
+		http.ServeFile(w, r, "index.html")
 
 	})
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
 
+		Login := r.FormValue("name")
+		Password := r.FormValue("password")
+
+		fmt.Printf("login is kaka - %v and the pass is kaka too - %v", Login, Password)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
+
+	http.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "regis.html")
+	})
+
+	http.HandleFunc("/REGsubmit", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+
+		NEW_LOGIN := r.FormValue("name")
+		NEW_PASSWORD := r.FormValue("password")
+		EMAIL := r.FormValue("email")
+
+		fmt.Printf(`NEW: 
+					login - %v  
+					pass - %v
+					email - %v`, NEW_LOGIN, NEW_PASSWORD, EMAIL)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
+	http.ListenAndServe(":8080", nil)
 }
