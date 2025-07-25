e85144806041f950cea6be6c79f0ba25808f8649
chelik <cheldobrek3541@gmail.com>
2025-07-25 18:45:50 +0300

    сделал обработчик для будущей формы на страницу логина. Добавил папку для прологов

--- server/servside/main.go
+++ server/servside/main.go
@@ -3,9 +3,9 @@ package main
 import "net/http"
 
 func main() {
-
+	//отдаем все статичные файлы(а то есть html,css,js)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./front"))))
-
+	//редирект с корня сайта
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/" {
 			http.Redirect(w, r, "./front/index.html", http.StatusSeeOther)
@@ -15,6 +15,16 @@ func main() {
 		}
 
 	})
+	//обработка страницы логина для получения последующих post запросов
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 
+		if r.URL.Path == "/login" {
+			http.ServeFile(w, r, "./front/login.html")
+		} else {
+			http.NotFound(w, r)
+		}
+
+	})
+	//запуск сервера
 	http.ListenAndServe(":8080", nil)
 }
