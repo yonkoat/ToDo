bb4656d361260011514c9a5a4929cc3e9e89cc1d
chelik <cheldobrek3541@gmail.com>
2025-07-29 17:17:19 +0300

    Сделал обработчик форм регистрации и логина. Пока что в ветке разроботки по причине неуверенности в долговечности. Завтра проверю и сделаю финальный мердж

--- server/servside/main.go
+++ server/servside/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	//отдаем все статичные файлы(а то есть html,css,js)
@@ -19,11 +22,20 @@ func main() {
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./front/index.html")
 	})
-
+	//Обработка страницы регистрации
 	http.HandleFunc("/registrate", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./front/registrate.html")
 	})
+	http.HandleFunc("/registrating", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
 
+		NEW_USERNAME := r.FormValue("username")
+		NEW_PASSWORD := r.FormValue("password")
+		EMAIL := r.FormValue("email")
+
+		fmt.Printf("REGISTRATION:\n USERNAME-%v\n Password-%v\n EMAIL-%v", NEW_USERNAME, NEW_PASSWORD, EMAIL)
+		http.Redirect(w, r, "/home", http.StatusSeeOther)
+	})
 	//обработка страницы логина для получения последующих post запросов
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 
@@ -33,6 +45,15 @@ func main() {
 			http.NotFound(w, r)
 		}
 
+	})
+	http.HandleFunc("/login_in", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+
+		Username := r.FormValue("username")
+		Password := r.FormValue("password")
+
+		fmt.Printf("LOGIN IN:\n USERNAME-%v\n Password-%v\n", Username, Password)
+		http.Redirect(w, r, "/home", http.StatusSeeOther)
 	})
 	//запуск сервера
 	http.ListenAndServe(":8080", nil)
