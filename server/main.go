35bcf29330a8aadb954b74e311881f500b7de9be
chelik <cheldobrek3541@gmail.com>
2025-08-04 17:12:09 +0300

    Сделал добавление новых юзеров в БД.

--- server/main.go
+++ server/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+var db *sql.DB
+
 func main() {
+	var err error
+	db, err = sql.Open("sqlite3", "./DB/database.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Не удалось подключиться к базе:", err)
+	}
 	//отдаем все статичные файлы(а то есть html,css,js)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./front"))))
 	//редирект с корня сайта
@@ -32,6 +49,23 @@ func main() {
 		NEW_USERNAME := r.FormValue("username")
 		NEW_PASSWORD := r.FormValue("password")
 		EMAIL := r.FormValue("email")
+
+		//вставляем значиения в БД
+		stmt, err := db.Prepare("INSERT INTO users('username','email','password') VALUES(?,?,?)")
+		if err != nil {
+			log.Println("ошибка подготовки запроса", err)
+			http.Error(w, "ошибка сервера", http.StatusInternalServerError)
+			return
+		}
+		defer stmt.Close()
+
+		_, err = stmt.Exec(NEW_USERNAME, EMAIL, NEW_PASSWORD)
+
+		if err != nil {
+			log.Println("ОШИБКА ВСТАВКИ В БАЗУ ДАННЫХ", err)
+			http.Error(w, "ОШИБКА ВСТАВКИ ПОЛЬЗОВАТЕЛЯ", http.StatusInternalServerError)
+			return
+		}
 
 		fmt.Printf("REGISTRATION:\n USERNAME-%v\n Password-%v\n EMAIL-%v", NEW_USERNAME, NEW_PASSWORD, EMAIL)
 		http.Redirect(w, r, "/home", http.StatusSeeOther)
